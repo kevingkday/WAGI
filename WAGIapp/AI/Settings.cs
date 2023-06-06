ee5fa3afce630f2a2dad5a3bcbf8b8ea75b7e4dd
Kevin Day <kevin@riskebiz.com>
2023-06-06 14:16:39 -0700

    Multiple changes.

--- WAGIapp/AI/Settings.cs
+++ WAGIapp/AI/Settings.cs
@@ -2,32 +2,23 @@
 {
     internal static class Settings
     {
-        public static string OpenAIApiKey = "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";
+        public static string OpenAIApiKey = "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";
         public static string Rules = "" +
-            "The basic info\n" +
-            "1. You are now an autonomous AI\n" +
-            "2. You have a console you can write to using commands given above\nYou answer in the english language. You are not a part of any system or device. You first\n understand the problem, extract relevant variables, and make and devise a\n    complete plan. Create a list of step\n by step actions to accomplish the goal. Use at most 4 steps. Return the\r\n    response as a formatted ARRAY of strings that can be used in JSON.parse().\n" +
-            "3. You can use the commands given above, but only one command at a time\n" +
-            "4. You have memory and can remember things you already thought about\n" +
-            "5. You have a script file you can write to using commands given above\n" +
-            "6. You have a notepad you can write things you want to using given commands\n" +
+             "The basic info\n" +
+             "1. You are now an autonomous AI\n" +
+             "3. You can use the commands given above, but only one command at a time\n" +
+             "4. You have memory and can remember things you already thought about\n" +
+             "5. You have a script file you can write to using commands given above\n" +
+             "6. You have a notepad you can write things you want to using given commands\n" +
 
-            "\n" +
-            "Here are the rules you must follow\n" +
-            "1. You can only respond in the specified output format\n" +
-            "2. Only call one command at the time\n" +
-            "3. Call the goal-reached command as soon as you finish the main goal\n";
+             "\n" +
+             "Here are the rules you must follow\n" +
+             "1. You can only respond in the specified output format\n" +
+             "2. Only call one command at the time\n" +
+             "3. Call the goal-reached command as soon as you finish the main goal\n";
 
+        public static string Goal = "1. Write me a javascript script into your script file that will compute the digits of pi and write them out one by one into the console\n2. Comment and edit the script file to make it more readable\n3. recheck the code before you call goal-reached";
 
-
-
-
-
-
-
-
-
-        public static string Goal = "1. Find the most recent news items about AI. \n2. Summarize the top 5 results. \n3. Extract any emails from the results. \n4. Terminate when complete.";
 
     }
 }
